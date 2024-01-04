60e63f3be1da526170fda27308d85cd0d5bf8692
Mahmoud Mourad <mahmoud.mourad.dev@gmail.com>
2024-01-04 11:53:12 -0800

    Add files via upload

---
+++ Boolean1 Example.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+contract boolExample{
+    // Defualt boolean Value False
+    // boolean variable return true or false
+    
+    bool isApproved;   
+    bool isMarried; 
+    bool isActive = true;
+    bool study = false;
+
+
+}
