2fa387cdf5db07e26218ef1a6ec51363a01717bf
Mahmoud Mourad <mahmoud.mourad.dev@gmail.com>
2024-01-04 11:53:37 -0800

    Add files via upload

---
+++ integerExample.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+contract integerExample{
+    int wheather= -2;
+    int airPodes;
+    uint iphonePrice;
+    uint8 macPrice=255; // only number(0-255)
+    uint16 ipadPrice= 65535; // only number(0-65535_
+    
+}
